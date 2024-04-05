50744d0c209ab52a7854e84375a67661aaafb264
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-06 01:51:12 +0530

    using loops to render a list 🧠

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -9,19 +9,24 @@ const App = () => {
   just put it inside the {} */
   
   /* you can't have "string" inside the {} */
-
+  const name = "dude";
   const x = 10;
   const y = 20;
-
+  const names = ['dude','bro','pal','homie','mate'];
   return  (
     <div>
       <h1>Hello</h1>
+      <h2> {name} </h2>
       <p>hello bro this is my react app</p>
       <p>sum of {x} and {y} is equal to {x+y}  </p>
+      <div>
+        <ul> {names.map((name ,index)=>(<li key={index}>
+          {name}
+        </li>))} </ul>
+      </div>
     </div>
   ) 
 }
-
 export default App;
 
 
